81c7f6ddc33dd2de6c7179fc3d0de5a5bea27773
david.shrader <dshrader38@gmail.com>
2020-02-14 16:28:39 -0600

    Initial commit

--- dot-files/bootstrap.sh
+++ dot-files/bootstrap.sh
@@ -11,15 +11,22 @@ fi
 
 # create dot-files symlinks
 ln -fs ~/cloud/dot-files/.bashrc ~/.bashrc
+echo ".bashrc symlink created"
 ln -fs ~/cloud/dot-files/.bash_profile ~/.bash_profile
+echo ".bash_profile symlink created"
 ln -fs ~/cloud/dot-files/.bash_logout ~/.bash_logout
+echo ".bash_logout symlink created"
 ln -fs ~/cloud/dot-files/.gitconfig ~/.gitconfig
+echo ".gitconfig symlink created"
 ln -fs ~/cloud/dot-files/.gitignore_global ~/.gitignore_global
+echo ".gitignore_global symlink created"
 #ln -fs ~/cloud/dot-files/.pip ~/.pip
 #ln -fs ~/cloud/dot-files/.tmux ~/.tmux
 #ln -fs ~/cloud/dot-files/.tmux.conf ~/.tmux.conf
 ln -fs ~/cloud/dot-files/.vim ~/.vim
+echo ".vim symlink created"
 ln -fs ~/cloud/dot-files/.vimrc ~/.vimrc
+echo ".vimrc symlink created"
 
 # set global git parameters
 #git config --global core.excludesfile '~/.gitignore_global'
